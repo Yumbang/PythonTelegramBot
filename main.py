cb3dddc23110128a6c1caec3897f9308bc0c0c3d
Yumbang <yumbang_ct@icloud.com>
2021-09-12 20:01:17 +0900

    Initiated echo handler and configured filters

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
 
 updater = Updater(token='1986430016:AAG37he-O23D6GegnOZWRNymBeYS45QiNgo', use_context = True)
 dispatcher = updater.dispatcher
@@ -11,8 +12,18 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = "Hello Human! I'm a bot!")
 
+def echo(update, context):
+    context.bot.send_message(chat_id = update.effective_chat.id, text = update.message.text)
+
+
+
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+echo_handler = MessageHandler(Filters.text &(~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+
 
 updater.start_polling()
 
