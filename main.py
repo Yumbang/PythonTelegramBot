3c3077491839e637d087b527e0f3cb952742c7ce
Yumbang <yumbang_ct@icloud.com>
2021-09-12 20:24:47 +0900

    Initiated caps handler. Caps function implement needed for words containing capitals

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import logging
 import telegram
 from telegram.ext import Updater
@@ -15,6 +16,9 @@ def start(update, context):
 def echo(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = update.message.text)
 
+def caps(update, context):
+    text_caps = ''.join(context.args).upper()
+    context.bot.send_message(chat_id = update.effective_chat.id, text = text_caps)
 
 
 start_handler = CommandHandler('start', start)
@@ -23,14 +27,12 @@ dispatcher.add_handler(start_handler)
 echo_handler = MessageHandler(Filters.text &(~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
 
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
 
 
 updater.start_polling()
-
-
-
-
-
 
 
 
