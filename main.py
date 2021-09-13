a60851fc94a4df45cd968c121d3cca607c6b6d9d
Yumbang <yumbang_ct@icloud.com>
2021-09-13 20:25:25 +0900

    Initiated inline query handler, but cannot know what it does. Imported os so that telegram token can be stored in file.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import time
+import os
 import logging
 import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import InlineQueryHandler
+
+
 
 updater = Updater(token='1986430016:AAG37he-O23D6GegnOZWRNymBeYS45QiNgo', use_context = True)
 dispatcher = updater.dispatcher
@@ -20,6 +24,22 @@ def caps(update, context):
     text_caps = ''.join(context.args).upper()
     context.bot.send_message(chat_id = update.effective_chat.id, text = text_caps)
 
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+            InlineQueryResultArticle(
+                    id = query.upper(),
+                    title = "Caps",
+                    input_message_content = InputTextMessageContent(query.upper())
+                )
+            )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
@@ -30,6 +50,8 @@ dispatcher.add_handler(echo_handler)
 caps_handler = CommandHandler('caps', caps)
 dispatcher.add_handler(caps_handler)
 
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
 
 
 updater.start_polling()
